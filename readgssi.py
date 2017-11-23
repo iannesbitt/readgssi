d2f518a92d5914451476b0b0857567562ec13c9d
Ian Nesbitt <ian.nesbitt@gmail.com>
2017-11-22 20:54:19 -0500

    working csv output

--- readgssi.py
+++ readgssi.py
@@ -15,11 +15,17 @@
 # a copy of the license upon obtaining this software, please visit
 # (https://opensource.org/licenses/MIT) to obtain a copy.
 
-import sys, getopt
+import sys, getopt, os
 import struct, bitstruct
 import numpy as np
+import pandas as pd
 from datetime import datetime
 import pytz
+
+NAME = 'readgssi'
+VERSION = '0.0.3-dev'
+AUTHOR = 'Ian Nesbitt'
+AFFIL = 'School of Earth and Climate Sciences, University of Maine'
 
 MINHEADSIZE = 1024 # absolute minimum total header size
 PAREASIZE = 128 # fixed info header size
@@ -57,13 +63,13 @@ def readbit(bits, start, end):
     '''
     function to read variables bitwise, where applicable
     '''
-    #try:
+    try:
         if start == 0:
             return bitstruct.unpack('<u'+str(end+1), bits)[0]
         else:
             return bitstruct.unpack('<p'+str(start)+'u'+str(end-start), bits)[0]
-    #except:
-        #print('error reading bits')
+    except:
+        print('error reading bits')
 
 def readtime(bits):
     '''
@@ -82,7 +88,7 @@ def readtime(bits):
         except:
             return datetime(1980,1,1,0,0,0,0,tzinfo=pytz.UTC) # most of the time the info returned is garbage, so we return arbitrary datetime again
 
-def readgssi(argv=None):
+def readgssi(argv=None, call=None):
     '''
     function to unpack and return things we need from the header, and the data itself
 
@@ -96,112 +102,128 @@ def readgssi(argv=None):
     '''
     infile = ''
     outfile = ''
-    help_text = 'readgssi.py -i <input file> -o <output file> -f <format: (csv|h5|segy)>' # help text string
+    frmt = ''
+    help_text = 'usage:\nreadgssi.py -i <input file> -o <output file> -f <format: (csv|h5|segy)>\n' # help text string
 
-    # parse passed command line arguments. this will probably move somewhere else.
+    # parse passed command line arguments. this may get moved somewhere else, but for now:
     try:
-        opts, args = getopt.getopt(argv,'hi:o:f:',['input=','output=','format='])
+        opts, args = getopt.getopt(argv,'hi:o:f:',['help','input=','output=','format='])
     # the 'no option supplied' error
     except getopt.GetoptError:
+        print('invalid argument(s) supplied')
         print(help_text)
         sys.exit(2)
     for opt, arg in opts: 
-        if opt == ('-h', '--help'): # the help case
+        if opt in ('-h', '--help'): # the help case
+            print(AUTHOR)
+            print(AFFIL + '\n')
             print(help_text)
             sys.exit()
-        elif opt in ('-i', '--input'): # the input file
-            if arg == '':
-                # this case probably won't happen but we'll catch it anyway
-                print('no file was given. please try again.')
-                print(help_text)
-                sys.exit(2)
-            else:
+        if opt in ('-i', '--input'): # the input file
+            if arg:
                 infile = arg
-        elif opt in ('-o', '--output'): # the output file
-            outfile = arg
-        elif opt in ('-f', '--format'): # the format string
+                if '~' in infile:
+                    infile = os.path.expanduser(infile) # if using --input=~/... tilde needs to be expanded 
+        if opt in ('-o', '--output'): # the output file
+            if arg:
+                outfile = arg
+                if '~' in outfile:
+                    outfile = os.path.expanduser(infile) # expand tilde, see above
+        if opt in ('-f', '--format'): # the format string
             # check whether the string is a supported format
-            if arg == ('csv', 'CSV'):
-                frmt = 'csv'
-            elif arg == ('sgy', 'segy', 'seg-y' 'SGY', 'SEGY', 'SEG-Y'):
-                frmt = 'segy'
-            elif arg == ('h5', 'hdf5', 'H5', 'HDF5'):
-                frmt = 'h5'
+            if arg:
+                if arg in ('csv', 'CSV'):
+                    frmt = 'csv'
+                elif arg in ('sgy', 'segy', 'seg-y', 'SGY', 'SEGY', 'SEG-Y'):
+                    frmt = 'segy'
+                elif arg in ('h5', 'hdf5', 'H5', 'HDF5'):
+                    frmt = 'h5'
+                else:
+                    # else the user has given an invalid format
+                    print(help_text)
+                    sys.exit(2)
             else:
-            	# else the user has given an invalid format
                 print(help_text)
                 sys.exit(2)
-    try:
-        with open(infile, 'rb') as f:
-            # open the binary, start reading chunks
-            rh_tag = struct.unpack('<h', f.read(2))[0] # 0x00ff if header, 0xfnff if old file format
-            rh_data = struct.unpack('<h', f.read(2))[0] # offset to data from beginning of file
-            rh_nsamp = struct.unpack('<h', f.read(2))[0] # samples per scan
-            rh_bits = struct.unpack('<h', f.read(2))[0] # bits per data word
-            rh_zero = struct.unpack('<h', f.read(2))[0] # if sir-30 or utilityscan df, then repeats per sample; otherwise 0x80 for 8bit and 0x8000 for 16bit
-            rhf_sps = struct.unpack('<f', f.read(4))[0] # scans per second
-            rhf_spm = struct.unpack('<f', f.read(4))[0] # scans per meter
-            rhf_mpm = struct.unpack('<f', f.read(4))[0] # meters per mark
-            rhf_position = struct.unpack('<f', f.read(4))[0] # position (ns)
-            rhf_range = struct.unpack('<f', f.read(4))[0] # range (ns)
-            rh_npass = struct.unpack('<h', f.read(2))[0] # number of passes for 2-D files
-            rhb_cdt = readtime(f.read(4)) # creation date and time in bits, structured as little endian u5u6u5u5u4u7
-            rhb_mdt = readtime(f.read(4)) # modification date and time in bits, structured as little endian u5u6u5u5u4u7
-            f.seek(44) # skip across some proprietary BS
-            rh_text = struct.unpack('<h', f.read(2))[0] # offset to text
-            rh_ntext = struct.unpack('<h', f.read(2))[0] # size of text
-            rh_proc = struct.unpack('<h', f.read(2))[0] # offset to processing history
-            rh_nproc = struct.unpack('<h', f.read(2))[0] # size of processing history
-            rh_nchan = struct.unpack('<h', f.read(2))[0] # number of channels
-            rhf_epsr = struct.unpack('<f', f.read(4))[0] # average dilectric
-            rhf_top = struct.unpack('<f', f.read(4))[0] # position in meters (useless?)
-            rhf_depth = struct.unpack('<f', f.read(4))[0] # range in meters (again-useless?)
-            #rhf_coordx = struct.unpack('<ff', f.read(8))[0] # this is definitely useless
-            f.seek(113) # skip to something that matters
-            vsbyte = f.read(1) # byte containing versioning bits
-            rh_version = readbit(vsbyte, 0, 2) # whether or not the system is GPS-capable (probably does not mean GPS is in file)
-            rh_system = readbit(vsbyte, 3, 7) # the system type (values in UNIT={...} dictionary above)
-            del vsbyte
+
+    if infile:
+        try:
+            with open(infile, 'rb') as f:
+                # open the binary, start reading chunks
+                rh_tag = struct.unpack('<h', f.read(2))[0] # 0x00ff if header, 0xfnff if old file format
+                rh_data = struct.unpack('<h', f.read(2))[0] # offset to data from beginning of file
+                rh_nsamp = struct.unpack('<h', f.read(2))[0] # samples per scan
+                rh_bits = struct.unpack('<h', f.read(2))[0] # bits per data word
+                rh_zero = struct.unpack('<h', f.read(2))[0] # if sir-30 or utilityscan df, then repeats per sample; otherwise 0x80 for 8bit and 0x8000 for 16bit
+                rhf_sps = struct.unpack('<f', f.read(4))[0] # scans per second
+                rhf_spm = struct.unpack('<f', f.read(4))[0] # scans per meter
+                rhf_mpm = struct.unpack('<f', f.read(4))[0] # meters per mark
+                rhf_position = struct.unpack('<f', f.read(4))[0] # position (ns)
+                rhf_range = struct.unpack('<f', f.read(4))[0] # range (ns)
+                rh_npass = struct.unpack('<h', f.read(2))[0] # number of passes for 2-D files
+                rhb_cdt = readtime(f.read(4)) # creation date and time in bits, structured as little endian u5u6u5u5u4u7
+                rhb_mdt = readtime(f.read(4)) # modification date and time in bits, structured as little endian u5u6u5u5u4u7
+                f.seek(44) # skip across some proprietary BS
+                rh_text = struct.unpack('<h', f.read(2))[0] # offset to text
+                rh_ntext = struct.unpack('<h', f.read(2))[0] # size of text
+                rh_proc = struct.unpack('<h', f.read(2))[0] # offset to processing history
+                rh_nproc = struct.unpack('<h', f.read(2))[0] # size of processing history
+                rh_nchan = struct.unpack('<h', f.read(2))[0] # number of channels
+                rhf_epsr = struct.unpack('<f', f.read(4))[0] # average dilectric
+                rhf_top = struct.unpack('<f', f.read(4))[0] # position in meters (useless?)
+                rhf_depth = struct.unpack('<f', f.read(4))[0] # range in meters (again-useless?)
+                #rhf_coordx = struct.unpack('<ff', f.read(8))[0] # this is definitely useless
+                f.seek(113) # skip to something that matters
+                vsbyte = f.read(1) # byte containing versioning bits
+                rh_version = readbit(vsbyte, 0, 2) # whether or not the system is GPS-capable (probably does not mean GPS is in file)
+                rh_system = readbit(vsbyte, 3, 7) # the system type (values in UNIT={...} dictionary above)
+                del vsbyte
 
 
-            if rh_data < MINHEADSIZE: # whether or not the header is normal or big-->determines offset to data array
-                f.seek(MINHEADSIZE * rh_data)
-            else:
-                f.seek(MINHEADSIZE * rh_nchan)
-            if rh_bits == 8:
-                data = np.fromfile(f, np.uint8).reshape(-1,rh_nsamp).T # 8-bit
-            elif rh_bits == 16:
-                data = np.fromfile(f, np.uint16).reshape(-1,rh_nsamp).T # 16-bit
-            else:
-                data = np.fromfile(f, np.int32).reshape(-1,rh_nsamp).T # 32-bit
+                if rh_data < MINHEADSIZE: # whether or not the header is normal or big-->determines offset to data array
+                    f.seek(MINHEADSIZE * rh_data)
+                else:
+                    f.seek(MINHEADSIZE * rh_nchan)
 
+                if rh_bits == 8:
+                    data = np.fromfile(f, np.uint8).reshape(-1,rh_nsamp).T # 8-bit
+                elif rh_bits == 16:
+                    data = np.fromfile(f, np.uint16).reshape(-1,rh_nsamp).T # 16-bit
+                else:
+                    data = np.fromfile(f, np.int32).reshape(-1,rh_nsamp).T # 32-bit
 
-            returns = {
-                'infile': infile,
-                'rh_system': rh_system,
-                'rh_version': rh_version,
-                'rh_nchan': rh_nchan,
-                'rh_nsamp': rh_nsamp,
-                'rh_bits': rh_bits,
-                'rhf_sps': rhf_sps,
-                'rhf_spm': rhf_spm,
-                'rhf_epsr': rhf_epsr,
-            }
+                # create returns dictionary
+                returns = {
+                    'infile': infile,
+                    'outfile': outfile,
+                    'frmt': frmt,
+                    'rh_system': rh_system,
+                    'rh_version': rh_version,
+                    'rh_nchan': rh_nchan,
+                    'rh_nsamp': rh_nsamp,
+                    'rh_bits': rh_bits,
+                    'rhf_sps': rhf_sps,
+                    'rhf_spm': rhf_spm,
+                    'rhf_epsr': rhf_epsr,
+                }
 
-            return returns, data
-    except IOError as e: # shows up when the user selects a file that doesn't exist or is inaccessable due to permissions error
-        print("i/o error: DZT file is inaccessable or does not exist, perhaps your spelling isn't what it used to be")
-        print('detail: ' + str(e))
+                return returns, data
+        except IOError as e: # when the user selects an inaccessible or nonexistent file
+            print("i/o error: DZT file is inaccessable or does not exist")
+            print('detail: ' + str(e) + '\n')
+            print(help_text)
+        
 
 if __name__ == "__main__":
     '''
-    this is the command-line use case
+    this is the command line use case
     '''
+    print(NAME + ' ' + VERSION)
     try:
         r = readgssi(argv=sys.argv[1:])
         rhf_sps = r[0]['rhf_sps']
         rhf_spm = r[0]['rhf_spm']
-        # print some useful things to command line users
+        # print some useful things to command line users from returned dictionary
         print('input file:         ' + r[0]['infile'])
         print('system:             ' + UNIT[r[0]['rh_system']])
         print('gps-enabled file:   ' + GPS[r[0]['rh_version']])
@@ -216,10 +238,24 @@ if __name__ == "__main__":
             print('seconds:            ' + str(r[1].shape[1]/rhf_sps))
         else:
             print('meters:             ' + str(r[1].shape[1]/rhf_spm))
-        if outfile != '':
-            print('output file:        ' + outfile)
-    except TypeError as e: # shows up when the user selects a file that doesn't exist
-        print('')
-        print('additionally, the console returned this typeerror: ' + str(e))
+        if r[0]['frmt']:
+            print('outputting to ' + r[0]['frmt'] + " . . .")
+            data = pd.DataFrame(r[1]) # using pandas to output csv
+            if r[0]['frmt'] in 'csv':
+                if r[0]['outfile']: # if output is given
+                    of = os.path.abspath(r[0]['outfile']) # set output to given location
+                else: # if no output is given
+                    # set output to the same dir as input file
+                    of = os.path.abspath(os.path.splitext(r[0]['infile'])[0] + '.' + r[0]['frmt'])
+                print('writing file to:    ' + of)
+                data.to_csv(of) 
+            elif r[0]['frmt'] in 'h5':
+                print('hdf5 is not yet supported, please choose another format.')
+            elif r[0]['frmt'] in 'segy':
+                print('SEG-Y is not yet supported, please choose another format.')
+            print('done.')
+        
+
+    except TypeError as e: # shows up when the user selects an input file that doesn't exist
         print('')
         sys.exit(2)
