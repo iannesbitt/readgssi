6ede2614fb339da68290be80caadc4de2de4458c
Ian Nesbitt <ian.nesbitt@gmail.com>
2020-09-14 21:58:59 -0400

    towards dzt write capability

--- readgssi/translate.py
+++ readgssi/translate.py
@@ -2,8 +2,10 @@ import h5py
 import pandas as pd
 import numpy as np
 import json
+import struct
 from readgssi.gps import readdzg
 import readgssi.functions as fx
+from datetime import datetime
 
 """
 contains translations to common formats
@@ -169,3 +171,110 @@ def h5(ar, infile_basename, outfile_abspath, header, verbose=False):
         eg.attrs.create(gutx, gutx_str) # store utm gpscluster attribute
         n += 1
     f.close()
+
+def writetime(d):
+    '''
+    Function to write dates to :code:`rfDateByte` binary objects in DZT headers.
+    An inverse of the :py:func:`readgssi.dzt.readtime` function.
+
+    DZT :code:`rfDateByte` objects are 32 bits of binary (01001010111110011010011100101111),
+    structured as little endian u5u6u5u5u4u7 where all numbers are base 2 unsigned int (uX)
+    composed of X number of bits. Four bytes is an unnecessarily high level of compression
+    for a single date object in a filetype that often contains tens or hundreds of megabytes
+    of array information anyway.
+
+    So this function reads a datetime object and outputs
+    (seconds/2, min, hr, day, month, year-1980).
+
+    For more information on :code:`rfDateByte`, see page 55 of
+    `GSSI's SIR 3000 manual <https://support.geophysical.com/gssiSupport/Products/Documents/Control%20Unit%20Manuals/GSSI%20-%20SIR-3000%20Operation%20Manual.pdf>`_.
+
+    :param datetime d: the :py:class:`datetime.datetime` to be encoded
+    :rtype: bytes
+    '''
+    # get binary values
+    sec2 = int(bin(int(d.second / 2))[2:])
+    mins = int(bin(d.minute)[2:])
+    hr = int(bin(d.hour)[2:])
+    day = int(bin(d.day)[2:])
+    mo = int(bin(d.month)[2:])
+    yr = int(bin(d.year - 1980)[2:])
+    # create binary string with proper padding
+    dtbits = '%07d%04d%05d%05d%06d%05d' % (yr, mo, day, hr, mins, sec2)
+    # create four bytes that make up rfDateByte
+    byt0 = int(dtbits[24:], 2)
+    byt1 = int(dtbits[16:24], 2)
+    byt2 = int(dtbits[8:16], 2)
+    byt3 = int(dtbits[0:8], 2)
+    # return a byte array
+    return bytes([byt0, byt1, byt2, byt3])
+
+def dzt(ar, outfile_abspath, header, verbose=False):
+    """
+    .. warning:: DZT output is not yet available.
+
+    In the future, this function will output to GSSI DZT format.
+
+    :param numpy.ndarray ar: Radar array
+    :param str infile_basename: Input file basename
+    :param str outfile_abspath: Output file path
+    :param dict header: File header dictionary to write, if desired. Defaults to None.
+    :param bool verbose: Verbose, defaults to False
+    """
+
+    '''
+    Assumptions:
+    - constant velocity between marks (may be possible to add a check)
+    '''
+    
+    outfile = open(outfile_abspath, 'wb')
+
+    for i in range(header['rh_nchan']):
+        outfile.write(struct.pack('<h', header['rh_tag']))
+        outfile.write(struct.pack('<h', header['rh_data']))
+        outfile.write(struct.pack('<h', header['rh_nsamp']))
+        outfile.write(struct.pack('<h', header['rh_bits']))
+        outfile.write(struct.pack('<h', header['rh_zero']))
+        # byte 10
+        outfile.write(struct.pack('<f', header['rhf_sps']))
+        outfile.write(struct.pack('<f', header['rhf_spm'])) # dzt.py ln 94-97
+        outfile.write(struct.pack('<f', header['rhf_mpm']))
+        outfile.write(struct.pack('<f', header['rhf_position']))
+        outfile.write(struct.pack('<f', header['rhf_range']))
+        outfile.write(struct.pack('<h', header['rh_npass']))
+        # byte 32
+        outfile.write(writetime(header['rhb_cdt']))
+        outfile.write(writetime(datetime.now())) # modification date/time
+        # byte 40
+        outfile.write(struct.pack('<h', header['rh_rgain']))
+        outfile.write(struct.pack('<h', header['rh_nrgain']))
+        outfile.write(struct.pack('<h', header['rh_text']))
+        outfile.write(struct.pack('<h', header['rh_ntext']))
+        outfile.write(struct.pack('<h', header['rh_proc']))
+        outfile.write(struct.pack('<h', header['rh_nproc']))
+        outfile.write(struct.pack('<h', header['rh_nchan']))
+        outfile.write(struct.pack('<f', header['rhf_epsr'])) # dzt.py ln 121-126
+        outfile.write(struct.pack('<f', header['rhf_top']))
+        outfile.write(struct.pack('<f', header['rhf_depth']))
+        # byte 66
+        outfile.write(struct.pack('<f', header['rh_xstart'])) # part of rh_coordx
+        outfile.write(struct.pack('<f', header('rh_xend'))) # part of rh_coordx
+        outfile.write(struct.pack('<f', header('rhf_servo_level')))
+        outfile.write(bytes(3)) # "reserved"
+        outfile.write(struct.pack('B', header['rh_accomp'])
+        outfile.write(struct.pack('<h', header['rh_sconfig']))
+        outfile.write(struct.pack('<h', header['rh_spp']))
+        outfile.write(struct.pack('<h', header['rh_linenum']))
+        # byte 88
+        outfile.write(struct.pack('<f', header['rh_ystart'])) # part of rh_coordy
+        outfile.write(struct.pack('<f', header['rh_yend'])) # part of rh_coordy
+        outfile.write(header['rh_96'])
+        outfile.write(struct.pack('c', header['rh_dtype']))
+        outfile.write(header['dzt_ant'][i])
+
+
+    infile.close()
+    outfile.close()
+
+
+int(bin(int(vsbyte, base=16)).lstrip('0b')[0:3], 2)
