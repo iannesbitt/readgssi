f2c60e871f7a0fbf05c7113bf19d28ef797edf31
Ian Nesbitt <ian.nesbitt@gmail.com>
2021-11-18 15:48:19 -0500

    updating license classifier

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     ''',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Framework :: Matplotlib",
         "Topic :: Scientific/Engineering :: Physics",
